99a5f5b88839f5a43e2d6edcdf0941f28a4991f9
matthew atkins <108462359+scout0773@users.noreply.github.com>
2024-04-29 18:30:29 -0400

    added specific filename when saving domains

--- func/sort.py
+++ func/sort.py
@@ -1,6 +1,9 @@
 import json
+from datetime import datetime
 
-def sort(available):
+def sort(available, domain):
     sort = sorted(available, key=lambda x: x['domain'])
-    with open('available.json', 'w') as file:
+    date = datetime.now().strftime("%m%d%Y")
+    filename = f"available_{domain}_{date}.json"
+    with open(filename, 'w') as file:
         json.dump(sort, file, indent=4)
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from func.sort import sort
 if __name__ == '__main__':
     domain = input("Try searching for a domain you like: ")
     check_availability = check(domain)
-    sort_domains = sort(check_availability)
+    sort_domains = sort(check_availability, domain)
