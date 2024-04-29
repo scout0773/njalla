db58674dc35a4879df7dca99215d7b7b9c39296f
matthew atkins <108462359+scout0773@users.noreply.github.com>
2024-04-29 05:18:06 -0400

    main.py initial commit, working on functions :3

---
+++ main.py
@@ -0,0 +1,7 @@
+from func.check import check
+from func.sort import sort
+
+if __name__ == '__main__':
+    domain = input("Try searching for a domain you like: ")
+    check_availability = check(domain)
+    sort_domains = sort(check_availability)
